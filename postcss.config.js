10ff9d61a32249c40970af6cf373f22f33475c0e
Julien Minguely <jminguely@gmail.com>
2021-08-24 08:52:13 +0200

    Styles the header and remove node modules

--- postcss.config.js
+++ postcss.config.js
@@ -12,6 +12,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 module.exports = {
   plugins: [
     require('tailwindcss'),
+    require('postcss-nested'),
     require('autoprefixer'),
     // ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
   ],
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
     content: ['./src/**/*.html'],
   },
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
     fontFamily: {
       'sans': ['"Noto Sans JP"', 'sans-serif'],
       'heading': ['Ellipse', 'sans-serif'],
