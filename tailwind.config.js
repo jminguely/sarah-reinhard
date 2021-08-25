00a3294466f175de5a6ce438e56d4f4f4446190d
Julien Minguely <jminguely@gmail.com>
2021-08-25 21:38:20 +0200

    Adds sections content

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,21 @@ module.exports = {
     content: ['./src/**/*.html'],
   },
   theme: {
+    colors: {
+      red: '#d51317',
+      white: '#ffffff',
+      black: '#1d1d1b',
+      grey: '#3f3f3d'
+    },
     container: {
       center: true,
       padding: '2rem',
+      screens: {
+        sm: "100%",
+        md: "100%",
+        lg: "720px",
+        xl: "900px"
+      }
     },
     fontFamily: {
       'sans': ['"Noto Sans JP"', 'sans-serif'],
